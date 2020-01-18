07583fb61354c4b668148a1aa90eb4f44cdd1b79
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-18 10:09:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ function mapToNoChange (sourceArray) {
   }
 return NewArr;
 }
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+  let total= startingPoint;
+  for(let i = 0;i < sourceArray.length; i++){
+   total  = total + sourceArray[i];
+}
+return total;
+}
+function reduceToAllTrue(sourceArray) {
+  for(let i = 0;i < sourceArray.length; i++){
+    if (!sourceArray[i])
+    {
+      return false;
+    }
+}
+return true;
+}
+function reduceToAnyTrue(sourceArray) {
+  for(let i = 0;i < sourceArray.length; i++){
+    if (sourceArray[i])
+    {
+      return true;
+    }
+}
+return false;
+}
