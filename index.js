7d40a55048a6538ebef09ead5355ae8d7a14043c
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-18 10:08:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function mapToNegativize (sourceArray) {
+  let NewArr =[];
+  for (let i =0 ; i< sourceArray.length; i++) {
+    NewArr.push(sourceArray[i]* -1) ;
+  }
+return NewArr;
+}
+function mapToNoChange (sourceArray) {
+  let NewArr =[];
+  for (let i =0 ; i< sourceArray.length; i++) {
+    NewArr.push(sourceArray[i]) ;
+  }
+return NewArr;
+}
